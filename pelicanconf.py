5e6e6793616ee2a3f6fdfbd55cdd5e33b13b92d8
Michael Polidori <spatiumestabsolutum@gmail.com>
2020-08-06 12:49:33 -0400

    Add custom-theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ PATH = 'content'
 TIMEZONE = 'America/Detroit'
 
 DEFAULT_LANG = 'en'
-THEME = '../../clean-blog-custom'
+THEME = 'custom-theme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,15 +20,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+GITHUB_URL = 'http://github.com/mpolidori'
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+## Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+#
+## Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
